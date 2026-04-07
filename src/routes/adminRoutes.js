3ceba6ed768c68fadbfe997ce3e7bdacc0a66a1b
aansyawaluddin <aansyawaluddin2003@gmail.com>
2026-04-07 13:57:32 +0800

    setup express

---
+++ src/routes/adminRoutes.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import { adminController } from '../controllers/adminController.js';
+
+const router = express.Router();
+
+router.post('/tim', adminController.createTim);
